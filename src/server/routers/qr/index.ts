db1b61bfaf5cf9cc14cf1feedf926a4643e3f1b8
Den Pol <selection4test@gmail.com>
2021-08-07 23:55:40 +0300

    qr doc updated

--- src/server/routers/qr/index.ts
+++ src/server/routers/qr/index.ts
@@ -1,7 +1,7 @@
 import express, { Express as IExpress } from 'express'
 import { tstGenerate } from './mws/tst.generate'
 import { tryAuthOnOtherDevice } from './mws/tst.try-auth-on-other-device'
-import { getLoggedMap } from './mws/tst.get-logged-map'
+import { getLoggedMap } from './mws/get-logged-map'
 import { generate } from './mws/generate'
 import { clearState } from './mws/reset-logged-map'
 import { swagger } from './mws/swagger'
@@ -14,8 +14,8 @@ const qrApi: IExpress = express()
 
 qrApi.get('/tst.generate', tstGenerate)
 qrApi.get('/tst.try-auth-on-other-device', tryAuthOnOtherDevice)
-qrApi.get('/tst.get-logged-map', getLoggedMap)
 
+qrApi.get('/get-logged-map', getLoggedMap)
 qrApi.post('/generate', generate)
 qrApi.get('/reset-logged-map', clearState)
 qrApi.use(
