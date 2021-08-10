751546fb7e5324bbe1f0498f94ce89231f7869f5
Den <selection4test@gmail.com>
2021-08-11 02:02:07 +0300

    fix

--- src/server/routers/gcs/mws/get-users-map.js
+++ src/server/routers/gcs/mws/get-users-map.js
@@ -1,8 +1,14 @@
+import base64 from 'base64'
+
 export const getUsersMap = async (req, res) => {
   try {
     const state = req.gcsUsersMapInstance.getState()
 
-    return res.status(200).json({ success: true, usersMap: state })
+    return res.status(200).json({
+      success: true,
+      usersMap: state,
+      _pravosleva: base64.encode('http://pravosleva.ru/express-helper/gcs/add-user'),
+    })
   } catch (err) {
     return res.status(500).json({ success: false, message: err.message || 'No err message' })
   }
