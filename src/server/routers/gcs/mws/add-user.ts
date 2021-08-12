704d0d2465e0641590467cc29157c36116db7d94
Den <selection4test@gmail.com>
2021-08-13 02:04:29 +0300

    fix

--- src/server/routers/gcs/mws/add-user.ts
+++ src/server/routers/gcs/mws/add-user.ts
@@ -21,25 +21,35 @@ export const addUser = async (req: IRequest & { gcsUsersMapInstance: UsersMapSin
   req.gcsUsersMapInstance.addUser({ userName, data: chatData })
 
   const uniqueKey: string = userName
-  const staticData = getStaticJSONSync(req.gcsStorageFilePath)
-  const ts = new Date().getTime()
-  let myNewData: any = { ...chatData }
-  const myOldData = staticData[uniqueKey]
-
-  if (!!myOldData) myNewData = { ...myOldData, ...myNewData }
-
-  if (myNewData?.count) {
-    const count = myNewData?.count
-
-    myNewData = { ...myNewData, count: count + 1, ts }
-  } else {
-    myNewData = { ...myNewData, count: 1, ts }
+  const getStorageFilePath = (from: any): string | null => {
+    switch (from) {
+      case 'gcs': return req.gcsStorageFilePath;
+      default: return null;
+    }
   }
+  const storageFilePath = getStorageFilePath(from)
 
-  staticData[uniqueKey] = myNewData
+  if (!!storageFilePath) {
+    const staticData = getStaticJSONSync(storageFilePath)
+    const ts = new Date().getTime()
+    let myNewData: any = { ...chatData }
+    const myOldData = staticData[uniqueKey]
 
-  // NOTE: Update global state
-  if (from === 'gcs') writeStaticJSONAsync(req.gcsStorageFilePath, staticData)
+    if (!!myOldData) myNewData = { ...myOldData, ...myNewData }
+
+    if (myNewData?.count) {
+      const count = myNewData?.count
+
+      myNewData = { ...myNewData, count: count + 1, ts }
+    } else {
+      myNewData = { ...myNewData, count: 1, ts }
+    }
+
+    staticData[uniqueKey] = myNewData
+
+    // NOTE: Update global state
+    writeStaticJSONAsync(storageFilePath, staticData)
+  }
 
   return res.status(200).json({ success: true })
 }
