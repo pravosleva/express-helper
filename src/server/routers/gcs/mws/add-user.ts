ac216ff51728aad660010d3e38055bfdad039f5a
Den <selection4test@gmail.com>
2021-08-12 21:43:51 +0300

    fix

--- src/server/routers/gcs/mws/add-user.ts
+++ src/server/routers/gcs/mws/add-user.ts
@@ -13,7 +13,7 @@ export const addUser = async (req: IRequest & { gcsUsersMapInstance: UsersMapSin
   if (_skipedParams.length > 0) {
     return res
       .status(401)
-      .json({ ok: false, message: `Missing required parameter${_skipedParams.length > 1 ? 's' : ''}: ${_skipedParams.join(', ')}` })
+      .json({ success: false, message: `Missing required parameter${_skipedParams.length > 1 ? 's' : ''}: ${_skipedParams.join(', ')}` })
   }
 
   const uniqueKey: string = userName
