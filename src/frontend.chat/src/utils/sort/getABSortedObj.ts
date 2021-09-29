84cf6a85707c31708ebc20615ba9580254007228
Den <selection4test@gmail.com>
2021-09-30 01:51:15 +0300

    fix

--- src/frontend.chat/src/utils/sort/getABSortedObj.ts
+++ src/frontend.chat/src/utils/sort/getABSortedObj.ts
@@ -37,7 +37,7 @@ export const getABSortedObj0 = (arr: string[], substr?: string): TRes => {
   return res
 }
 
-export const getABSortedObj = (arr: string[], substr?: string): TRes => {
+export const getABSortedObj1 = (arr: string[], substr?: string): TRes => {
   const res: TRes = {}
 
   arr.forEach((str: string) => {
@@ -65,6 +65,44 @@ export const getABSortedObj = (arr: string[], substr?: string): TRes => {
         res[firstChar].splice(targetIndex, 0, str)
       }
     }
+  })
+
+  return res
+}
+
+export const getABSortedObj = (arr: string[], substr?: string): TRes => {
+  const res: TRes = {}
+  const possibleChars = '0123456789abcdefghijklmnopqrstuvwxyz'.split('')
+
+  possibleChars.forEach((char: string) => {
+    arr.forEach((str: string) => {
+      const firstChar = str[0].toLowerCase()
+
+      if (firstChar === char) {
+        if (!!substr) {
+          if (str.includes(substr.toLowerCase())) {
+            if (!res[firstChar]) {
+              res[firstChar] = [str]
+            } else {
+              const targetIndex = binarySearchIndex(res[firstChar], str)
+        
+              res[firstChar].splice(targetIndex, 0, str)
+            }
+          } else {
+            // NOTHING
+          }
+        } else {
+          if (!res[firstChar]) {
+            res[firstChar] = [str]
+          } else {
+            // res[firstChar].push(str)
+            const targetIndex = binarySearchIndex(res[firstChar], str)
+      
+            res[firstChar].splice(targetIndex, 0, str)
+          }
+        }
+      }
+    })
   })
 
   return res
