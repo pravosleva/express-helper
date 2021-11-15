7027245963ee24d0da9c83490a30bd4dd9998008
Den <selection4test@gmail.com>
2021-11-15 22:52:16 +0300

    build fix

--- src/set-front.signin-env.sh
+++ src/set-front.signin-env.sh
@@ -10,12 +10,12 @@ then
     ;;
     "prod")
 ## PRAVOSLEVA.RU
-#       echo "REACT_APP_API_URL=/express-helper
-# REACT_APP_EXTERNAL_ROUTING=/express-helper" > src/frontend.signin/.env.production
+      echo "REACT_APP_API_URL=/express-helper
+REACT_APP_EXTERNAL_ROUTING=/express-helper" > src/frontend.signin/.env.production
 
 ## SMARTPRICE beta2
-      echo "REACT_APP_API_URL=http://104.248.201.86:5000
-REACT_APP_EXTERNAL_ROUTING=http://104.248.201.86:5000" > src/frontend.signin/.env.production
+#       echo "REACT_APP_API_URL=http://104.248.201.86:5000
+# REACT_APP_EXTERNAL_ROUTING=http://104.248.201.86:5000" > src/frontend.signin/.env.production
     ;;
     *)
     echo "☠️ SCRIPT: unknown param $1" &&
