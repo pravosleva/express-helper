5af76f2159c71ca5c2aad0e45d89c0fd4a892c3e
Den Pol <selection4test@gmail.com>
2022-04-15 21:09:45 +0300

    tmp: nm dir added to deploy

--- deploy2.pravosleva.prod.sh
+++ deploy2.pravosleva.prod.sh
@@ -1,7 +1,9 @@
 deploy_dist_dir=root@pravosleva.ru:/home/projects/pravosleva-blog/express-helper/server-dist
+deploy_nm=root@pravosleva.ru:/home/projects/pravosleva-blog/express-helper/node_modules
 
 echo '-- DEPLOY STARTED' &&
 
 rsync -av --delete server-dist/ $deploy_dist_dir &&
+rsync -av --delete node_modules/ $deploy_nm &&
 
 echo '-- DEPLOY COMPLETED'
