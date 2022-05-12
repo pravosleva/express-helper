6b9eede1b1f5a63eb6fe825f6a516bbd342220a2
Den <selection4test@gmail.com>
2022-05-13 00:35:15 +0300

    test fix

--- src/__tests__/binarySearch.spec.js
+++ src/__tests__/binarySearch.spec.js
@@ -1,7 +1,8 @@
 /* eslint-disable jest/no-disabled-tests */
 import { binarySearchTsIndex } from '../server/utils/binarySearch.ts'
 import fakeData from './fake-data/binarySearch.set-01.json'
-import { sortByTs } from '../server/utils/sortByTs'
+import fakeDataSorted from './fake-data/binarySearch.set-02-sorted.json'
+// import { sortByTs } from '../server/utils/sortByTs'
 
 describe('Search index', () => {
   test.skip('it should be correct', () => {
@@ -13,11 +14,12 @@ describe('Search index', () => {
     expect(i2).toEqual(i1)
   })
 
-  test.skip('if array should be sorted', () => {
-    const roomData = fakeData.data['ux-test']
-    const json1 = JSON.stringify(roomData)
-    const json2 = JSON.stringify(roomData.sort(sortByTs))
+  test('binarySearchTsIndex', () => {
+    const roomData = fakeDataSorted.data['ux-test']
+    const targetTs = 1645164716822
+    const i1 = binarySearchTsIndex({ messages: roomData, targetTs })
+    const i2 = roomData.findIndex(({ ts }) => ts === targetTs)
 
-    expect(json2).toEqual(json1)
+    expect(i2).toEqual(i1)
   })
 })
