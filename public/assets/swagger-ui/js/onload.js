9798f441fd3355aa5cd0be8b59edbdd4760d1457
Den <selection4test@gmail.com>
2024-08-08 10:34:14 +0300

    swagger ui fix

--- public/assets/swagger-ui/js/onload.js
+++ public/assets/swagger-ui/js/onload.js
@@ -9,6 +9,10 @@
         topBar.style.boxShadow =
           '0px 2px 4px -1px rgba(0,0,0,0.2),0px 4px 5px 0px rgba(0,0,0,0.14),0px 1px 10px 0px rgba(0,0,0,0.12)'
         topBar.style.backgroundColor = '#FFF'
+        // topBar.style.position = 'fixed'
+        // topBar.style.top = '0'
+        // topBar.style.width = '100%'
+        // topBar.style.zIndex = '1'
       }
 
       const topbarElms = document.getElementsByClassName('topbar-wrapper')
